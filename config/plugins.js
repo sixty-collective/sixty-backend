29f446294aace95b7e78b8fb38e6334956bab0be
alexwen.creates@gmail.com <alexwen.creates@gmail.com>
2023-09-27 22:41:13 -0500

    Update plugins.

--- config/plugins.js
+++ config/plugins.js
@@ -1,27 +1,4 @@
 module.exports = ({ env }) => ({
-  "fuzzy-search": {
-    enabled: true,
-    config: {
-      contentTypes: [
-        {
-          uid: "api::profile.profile",
-          modelName: "profile",
-          transliterate: false,
-          fuzzysortOptions: {
-            characterLimit: 300,
-            threshold: -600,
-            limit: 10,
-            keys: [
-              {
-                name: "name",
-                weight: 100,
-              },
-            ],
-          },
-        },
-      ],
-    },
-  },
   "import-export-entries": {
     enabled: true,
     config: {},
