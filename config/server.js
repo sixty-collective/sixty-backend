2e09aebef9daf89ae71ff4e7c3253ecc4ce889d6
alexwen.creates@gmail.com <alexwen.creates@gmail.com>
2023-09-28 00:28:06 -0500

    Add host and port.

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  // host: env('HOST', '0.0.0.0'),
-  // port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   url: env("RENDER_EXTERNAL_URL"),
   app: {
     keys: env.array("APP_KEYS"),
